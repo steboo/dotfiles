67828ebdd0cd3c2a8a3d40f18a1cc69abebdb6bb
Stephen Booher <stephen.booher@gmail.com>
2016-09-23 16:23:09 -0600

    move vimrc and gvimrc into .vim directory

--- install.ps1
+++ install.ps1
@@ -2,7 +2,10 @@
 param(
   [parameter()]
   [string]
-  $TargetDirectory
+  $TargetDirectory,
+  [parameter()]
+  [switch]
+  $DownloadFromInternet
 )
 Set-StrictMode -Version 2.0
 $ErrorActionPreference = "Stop"
@@ -16,8 +19,33 @@ if (!$TargetDirectory) {
     }
 }
 
+# Create directories if necessary
 if (-not (Test-Path $TargetDirectory)) {
     mkdir $TargetDirectory
+}
+
+if (-not (Test-Path $TargetDirectory\autoload)) {
+    mkdir $TargetDirectory\autoload
+}
+
+if (-not (Test-Path $TargetDirectory\bundle)) {
+    mkdir $TargetDirectory\bundle
+}
+
+if (-not (Test-Path $TargetDirectory\colors)) {
+    mkdir $TargetDirectory\colors
+}
+
+if (-not (Test-Path $TargetDirectory\indent)) {
+    mkdir $TargetDirectory\indent
+}
+
+if (-not (Test-Path $TargetDirectory\swap)) {
+    mkdir $TargetDirectory\swap
+}
+
+if (-not (Test-Path $TargetDirectory\undo)) {
+    mkdir $TargetDirectory\undo
 }
 
 # Backup existing
@@ -30,7 +58,20 @@ if (Test-Path $TargetDirectory\gvimrc) {
 }
 
 # Copy new files
-Copy-Item .vimrc $TargetDirectory\vimrc
-Copy-Item .gvimrc $TargetDirectory\gvimrc
+Copy-Item .vim\vimrc $TargetDirectory\vimrc
+Copy-Item .vim\gvimrc $TargetDirectory\gvimrc
+Copy-Item .vim\indent\*.vim $TargetDirectory\indent\
 
 "Copied files to $TargetDirectory"
+
+if ($DownloadFromInternet) {
+  "Downloading vim plugins from the Internet..."
+  wget https://tpo.pe/pathogen.vim -OutFile $TargetDirectory\autoload\pathogen.vim
+
+  Push-Location $TargetDirectory\bundle
+  &git clone git://github.com/altercation/vim-colors-solarized.git
+  &git clone git://github.com/tpope/vim-dispatch.git
+  &git clone git://github.com/PProvost/vim-ps1.git
+  &git clone git://github.com/jpo/vim-railscasts-theme.git
+  Pop-Location
+}
